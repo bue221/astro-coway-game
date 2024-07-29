0b753521b3b81315ca731e96c576af2d813ad7b1
bue221 <camiloplaza3@gmail.com>
2024-07-29 03:17:54 -0500

    feat: Update index page to use GameContainer only
    - Removed the <h1> element from the main section of the index page
    - Updated the GameContainer component to use the 'client:only' prop
    
    This change improves the user experience by removing unnecessary heading and optimizing the rendering of the GameContainer component.

--- src/components/react/Containers/GameContainer.tsx
+++ src/components/react/Containers/GameContainer.tsx
@@ -1,7 +1,13 @@
-import { useCallback, useRef, useState } from "react";
-import { AROUND_CELLS, DEFAULT_GRID_SIZE } from "../../../lib/constants/game";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import {
+  AROUND_CELLS,
+  DEFAULT_GRID_SIZE,
+  SPEEDS,
+} from "../../../lib/constants/game";
 import useGrid from "../Hooks/useGrid";
+import useIsMobile from "../Hooks/useIsMobile";
 
+import { cn } from "@/lib/utils";
 import { Button } from "../../ui/button";
 import {
   Dialog,
@@ -11,41 +17,56 @@ import {
   DialogTrigger,
 } from "../../ui/dialog";
 import { ScrollArea } from "../../ui/scroll-area";
+import { Slider } from "../../ui/slider";
 
 const GameContainer = () => {
+  const isMobile = useIsMobile();
+
+  const DEFAULT_SIZE_DEVICE = useMemo(() => {
+    return isMobile ? 18 : DEFAULT_GRID_SIZE;
+  }, [isMobile]);
+
   const { grid, setGrid, toggleCell, generateEmptyGrid } =
     useGrid(DEFAULT_GRID_SIZE);
 
   const [running, setRunning] = useState(false);
   const [generation, setGeneration] = useState(0);
   const [history, setHistory] = useState<any>([]);
+  const [speed, setSpeed] = useState(3);
 
   const runningRef = useRef(running);
   runningRef.current = running;
 
+  const latestGeneration = useRef<number[][]>([]);
+
   const runSimulation = useCallback(() => {
-    if (grid.every((row) => row.every((cell) => cell === 0))) {
-      console.log("All cells are dead", grid);
+    // Stop the simulation if the grid is empty
+    if (
+      latestGeneration?.current?.length > 0 &&
+      latestGeneration?.current?.every((row) => row.every((cell) => cell === 0))
+    ) {
       return;
     }
 
+    // Stop the simulation if the user stops it
     if (!runningRef.current) {
       return;
     }
 
+    // Run the simulation
     setGrid((g) => {
       const newGrid = g.map((row) => [...row]);
-      for (let i = 0; i < DEFAULT_GRID_SIZE; i++) {
-        for (let k = 0; k < DEFAULT_GRID_SIZE; k++) {
+      for (let i = 0; i < DEFAULT_SIZE_DEVICE; i++) {
+        for (let k = 0; k < DEFAULT_SIZE_DEVICE; k++) {
           let neighbors = 0;
           AROUND_CELLS.forEach(([x, y]) => {
             const newI = i + x;
             const newK = k + y;
             if (
               newI >= 0 &&
-              newI < DEFAULT_GRID_SIZE &&
+              newI < DEFAULT_SIZE_DEVICE &&
               newK >= 0 &&
-              newK < DEFAULT_GRID_SIZE
+              newK < DEFAULT_SIZE_DEVICE
             ) {
               neighbors += g[newI][newK];
             }
@@ -59,13 +80,16 @@ const GameContainer = () => {
         }
       }
       setHistory((history: any) => [...history, newGrid]);
+      latestGeneration.current = newGrid;
       return newGrid;
     });
 
+    // Update the generation
     setGeneration((generation) => generation + 1);
 
-    setTimeout(runSimulation, 100);
-  }, [grid]);
+    // Run the simulation again
+    setTimeout(runSimulation, SPEEDS[speed ?? 1]);
+  }, [grid, speed]);
 
   const onToggleStart = () => {
     setRunning(!running);
@@ -82,7 +106,7 @@ const GameContainer = () => {
   const onClear = () => {
     setHistory([]);
     setGeneration(0);
-    setGrid(generateEmptyGrid(DEFAULT_GRID_SIZE));
+    setGrid(generateEmptyGrid(DEFAULT_SIZE_DEVICE));
   };
 
   const onRandom = () => {
@@ -102,14 +126,26 @@ const GameContainer = () => {
     setGeneration(index + 1);
   };
 
+  useEffect(() => {
+    setGrid(generateEmptyGrid(DEFAULT_SIZE_DEVICE));
+  }, [isMobile]);
+
+  const handleChangeSpeed = (value: any) => {
+    setSpeed(value);
+  };
+
   return (
     <div className={"container mx-auto p-4 h-[80vh]"}>
-      <div className={"flex flex-col items-center space-y-4 gap-2"}>
-        <h1 className={"text-2xl"}>Generation: {generation}</h1>
+      <div
+        className={
+          "flex items-center justify-evenly flex-col-reverse md:flex-row p-4 "
+        }
+      >
         <div
+          className="mt-10 md:mt-0"
           style={{
             display: "grid",
-            gridTemplateColumns: `repeat(${DEFAULT_GRID_SIZE}, 20px)`,
+            gridTemplateColumns: `repeat(${DEFAULT_SIZE_DEVICE}, 20px)`,
           }}
         >
           {grid.map((rows, i) =>
@@ -117,47 +153,68 @@ const GameContainer = () => {
               <div
                 key={`${i}-${k}`}
                 onClick={handleToggleCell(i, k)}
-                style={{
-                  width: 20,
-                  height: 20,
-                  backgroundColor: grid[i][k] ? "pink" : undefined,
-                  border: "solid 1px black",
-                }}
+                className={cn(
+                  "cursor-pointer size-[20px] border border-black dark:border-slate-400 rounded-md",
+                  grid[i][k] ? "bg-pink-400 dark:bg-purple-600" : grid[i][k]
+                )}
               />
             ))
           )}
         </div>
-        <div className={"flex space-x-2"}>
-          <Button onClick={onRandom}>Random</Button>
-          <Button onClick={onClear}>Clear</Button>
-          <Button onClick={onToggleStart}>{running ? "Stop" : "Start"}</Button>
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button>History</Button>
-            </DialogTrigger>
-            <DialogContent className={"sm:max-w-[425px]"}>
-              <DialogHeader>
-                <DialogTitle>Generation History</DialogTitle>
-              </DialogHeader>
-              <ScrollArea
-                className={"h-[200px] w-full rounded-md border p-4"}
-                style={{ height: "400px" }}
-              >
-                {history.map((gen: any, index: number) => (
-                  <Button
-                    key={index}
-                    className={"w-full justify-start"}
-                    onClick={onSelectedGeneration(index)}
-                  >
-                    Generation {index + 1}
-                  </Button>
-                ))}
-                {history.length === 0 && (
-                  <p className={"text-center"}>No generations to show</p>
-                )}
-              </ScrollArea>
-            </DialogContent>
-          </Dialog>
+        <div className={"flex flex-col justify-center items-center gap-10"}>
+          <h1 className={"text-2xl font-bold italic"}>
+            Generation: {generation}
+          </h1>
+          <div className="w-full">
+            <label
+              htmlFor="speed"
+              className="block text-sm font-medium dark:text-white text-gray-700 mb-3"
+            >
+              Speed
+            </label>
+            <Slider
+              max={3}
+              min={1}
+              step={speed}
+              onValueChange={handleChangeSpeed}
+            />
+          </div>
+          <div className="flex gap-4">
+            <Button onClick={onRandom}>Random</Button>
+            <Button onClick={onToggleStart} variant="secondary">
+              {running ? "Stop" : "Start"}
+            </Button>
+            <Button onClick={onClear} variant="destructive">
+              Clear
+            </Button>
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button>History</Button>
+              </DialogTrigger>
+              <DialogContent className={"sm:max-w-[425px]"}>
+                <DialogHeader>
+                  <DialogTitle>Generation History</DialogTitle>
+                </DialogHeader>
+                <ScrollArea
+                  className={"h-[200px] w-full rounded-md border p-4"}
+                  style={{ height: "400px" }}
+                >
+                  {history.map((gen: any, index: number) => (
+                    <Button
+                      key={index}
+                      className={"w-full justify-start"}
+                      onClick={onSelectedGeneration(index)}
+                    >
+                      Generation {index + 1}
+                    </Button>
+                  ))}
+                  {history.length === 0 && (
+                    <p className={"text-center"}>No generations to show</p>
+                  )}
+                </ScrollArea>
+              </DialogContent>
+            </Dialog>
+          </div>
         </div>
       </div>
     </div>
